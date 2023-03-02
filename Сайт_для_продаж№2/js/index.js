2ee52d032491598f3b467f3930e2868c123ee8df
Egor6052 <101550518+Egor6052@users.noreply.github.com>
2023-03-02 14:47:11 +0200

    BIG UPDATE
    Oll site update

--- Сайт_для_продаж№2/js/index.js
+++ Сайт_для_продаж№2/js/index.js
@@ -1,18 +1,4 @@
 // $('.menu-btn').on('click', function(e) {
 //     e.preventDefault();
-//     $('.menu').toggleClass('menu_active');
+//     $('.menu').toggleClass('menu-active');
 // })
-
-let comments = [];
-
-document.getElementById('comment-add').onclick = function() {
-    let commentName = document.getElementById('comment-name');
-    let commentBodu = document.getElementById('comment-body');
-
-    let comment = {
-        name: commentName.value,
-        body: commentBodu.value,
-        time: Math.floor(Date.now() / 1000)
-    }
-    console.log(comment);
-};
